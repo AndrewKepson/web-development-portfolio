5f580474d8357fe1698ead2fb8e3d67aa61449bf
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-08-30 10:32:03 -0600

    Updated styling for consistency across site.

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Project = ({ item: { img, alt, title, descrip, href } }) => {
+const Project = ({ item: { img, alt, title, descrip, href, deployed } }) => {
     return(
-        <div className="card" style={{width: "20rem", height: "auto"}}>
+        <div className="card" style={{width: "20rem", height: "40rem"}}>
             <img src={img} className="card-img-top" alt={alt} />
             <div className="card-body">
             <h2 className="card-title">{title}</h2>
             <p className="card-text">{descrip}</p>
-            <a href={href} class="btn btn-light" target="_blank">View Project</a>
+            <col-sm-12>
+            <a href={href} className="btn btn-light" target="_blank">View Code</a> 
+            <a href={deployed} className="btn btn-light" target="_blank">View Project</a>
+            </col-sm-12>
             </div>
         </div>
     )
