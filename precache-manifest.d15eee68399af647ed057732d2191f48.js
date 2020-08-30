de22ddfd9902ccf94970c54f3a6bf2f2f8ee22b0
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-08-30 10:40:31 -0600

    Updates

---
+++ precache-manifest.d15eee68399af647ed057732d2191f48.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aedff449fc44de5c5cdd365b79526595",
+    "url": "/web-development-portfolio/index.html"
+  },
+  {
+    "revision": "ac5374ba5e1edc8b3fc7",
+    "url": "/web-development-portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "380698397a29e77d8d23",
+    "url": "/web-development-portfolio/static/css/main.97b52c09.chunk.css"
+  },
+  {
+    "revision": "ac5374ba5e1edc8b3fc7",
+    "url": "/web-development-portfolio/static/js/2.e7c71015.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-development-portfolio/static/js/2.e7c71015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380698397a29e77d8d23",
+    "url": "/web-development-portfolio/static/js/main.1b6d86e9.chunk.js"
+  },
+  {
+    "revision": "321eb8b99b8e8128d828",
+    "url": "/web-development-portfolio/static/js/runtime-main.853f0da5.js"
+  },
+  {
+    "revision": "9455634311f4e488a03df22729f580ef",
+    "url": "/web-development-portfolio/static/media/brewgle.94556343.png"
+  },
+  {
+    "revision": "d47a62f8e8fe4cb63fea8e60f29443e3",
+    "url": "/web-development-portfolio/static/media/employeetracker.d47a62f8.png"
+  },
+  {
+    "revision": "7c834a3d35e98f21fdf52c3c127f0a7d",
+    "url": "/web-development-portfolio/static/media/interplanetarytravel.7c834a3d.png"
+  },
+  {
+    "revision": "b2b08fa839698939b25cfe9a476fe172",
+    "url": "/web-development-portfolio/static/media/landingpage.b2b08fa8.png"
+  },
+  {
+    "revision": "1563b7e6bcde389148e613b6693487d6",
+    "url": "/web-development-portfolio/static/media/workouttracker.1563b7e6.png"
+  }
+]);
